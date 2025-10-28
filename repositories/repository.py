5489d5d284bccdfacebceb2280e0a8dc3e5e58b4
wyd33v <dev.vovas.whi33y@gmail.com>
2025-10-28 14:20:20 +0100

    created one generic repo

---
+++ repositories/repository.py
@@ -0,0 +1,30 @@
+from typing import Generic, TypeVar, Type, Optional, List
+from sqlalchemy.orm import Session
+from models.base import DBase
+
+T = TypeVar("T", bound=DBase)
+
+
+class Repository(Generic[T]):
+    def __init__(self, model: Type[T], db: Session):
+        self.model = model
+        self.db = db
+
+    def save(self, obj: T) -> T:
+        self.db.add(obj)
+        self.db.commit()
+        self.db.refresh(obj)
+        return obj
+
+    def delete(self, obj: T) -> None:
+        self.db.delete(obj)
+        self.db.commit()
+
+    def get_all(self) -> List[T]:
+        return self.db.query(self.model).all()
+
+    def get_by_id(self, pk: int) -> Optional[T]:
+        return self.db.query(self.model).get(pk)
+
+    def get_by_param(self, **filters) -> Optional[T]:
+        return self.db.query(self.model).filter_by(**filters).one_or_none()
